3fc492311b9a0c322679bb4eac33fd98121bf693
lina5274 <167440004+lina5274@users.noreply.github.com>
2024-09-23 13:32:39 +0300

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -13,7 +13,35 @@ class ProductBase(BaseModel):
     price: float
     quantity: int
 
-class ProductCreate(ProductBase):
+class ProductCreafrom pydantic import BaseModel, Field
+from typing import List
+from models import OrderStatus
+from datetime import datetime
+
+
+class ProductCreate(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    quantity: int
+
+class Product(ProductCreate):
+    id: int
+
+class OrderCreate(BaseModel):
+    products: List[ProductCreate]
+
+class Order(OrderCreate):
+    id: int
+    creation_date: datetime
+    status: OrderStatus
+
+class ProductUpdate(BaseModel):
+    name: str | None = None
+    description: str | None = None
+    price: float | None = None
+    quantity: int | None = None
+te(ProductBase):
     pass
 
 class Product(ProductBase):
